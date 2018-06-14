1cb3e8c97712edfad58929b2f38df8018d2a8488
Matty B <matthew.ian.barker@gmail.com>
2018-06-14 12:03:57 +0100

    Update fiveEighteenChess.java

--- fiveEighteenChess.java
+++ fiveEighteenChess.java
@@ -11,52 +11,52 @@ public class fiveEighteenChess
           StdDraw.enableDoubleBuffering();
           
 
-          for (int i=0; i<squares; i++)
+          for (int i=0; i<squares; i++)//Coloumns
                   {
-          if(squares%2==0)
-          {
-          for (int j=0; j<squares; j++)
-          {
+                  if(squares%2==0)//To check whether to add one if grid size is even
+                        {
+                        for (int j=0; j<squares; j++)//Rows
+                        {
 
-                   if(colour%2!=0)
-                   {
-                   StdDraw.setPenColor(StdDraw.RED);
-                   StdDraw.filledSquare((-0.8+(2*i*divs)),(-0.8+(2*j*divs)), divs);
-                   StdDraw.show();
-                   }
+                              if(colour%2!=0)
+                              {
+                              StdDraw.setPenColor(StdDraw.RED);
+                              StdDraw.filledSquare((-0.8+(2*i*divs)),(-0.8+(2*j*divs)), divs);
+                              StdDraw.show();
+                              }
                    
-                   else if(colour%2==0)
-                   {
-                   StdDraw.setPenColor(StdDraw.WHITE);
-                   StdDraw.filledSquare((-0.8+(2*i*divs)),(-0.8+(2*j*divs)), divs);
-                   StdDraw.show();
-                   }
-                   colour++;
-                   }
-          colour++;
-          }
+                         else if(colour%2==0)
+                              {
+                              StdDraw.setPenColor(StdDraw.WHITE);
+                              StdDraw.filledSquare((-0.8+(2*i*divs)),(-0.8+(2*j*divs)), divs);
+                              StdDraw.show();
+                              }
+                        colour++;
+                        }
+                  colour++;
+                  }
           
-          else
-          {
-                    for (int j=0; j<squares; j++)
-          {
+                  else
+                        {
+                        for (int j=0; j<squares; j++)
+                              {
 
-                   if(colour%2!=0)
-                   {
-                   StdDraw.setPenColor(StdDraw.RED);
-                   StdDraw.filledSquare((-0.8+(2*i*divs)),(-0.8+(2*j*divs)), divs);
-                   StdDraw.show();
-                   }
+                              if(colour%2!=0)
+                                    {
+                                    StdDraw.setPenColor(StdDraw.RED);
+                                    StdDraw.filledSquare((-0.8+(2*i*divs)),(-0.8+(2*j*divs)), divs);
+                                    StdDraw.show();
+                                    }
                    
-                   else if(colour%2==0)
-                   {
-                   StdDraw.setPenColor(StdDraw.WHITE);
-                   StdDraw.filledSquare((-0.8+(2*i*divs)),(-0.8+(2*j*divs)), divs);
-                   StdDraw.show();
-                   }
-                   colour++;
-                   }
-          }
+                              else if(colour%2==0)
+                                    {
+                                    StdDraw.setPenColor(StdDraw.WHITE);
+                                    StdDraw.filledSquare((-0.8+(2*i*divs)),(-0.8+(2*j*divs)), divs);
+                                    StdDraw.show();
+                                    }
+                              colour++;
+                              }
+                        }
           }
       }
 }
