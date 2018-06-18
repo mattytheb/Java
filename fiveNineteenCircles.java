faf0156516b0925cc4b894a69ff35446474fbd7d
Matty B <matthew.ian.barker@gmail.com>
2018-06-18 15:47:00 +0100

    Add files via upload

---
+++ fiveNineteenCircles.java
@@ -0,0 +1,48 @@
+public class fiveNineteenCircles
+{
+      public static void main(String[] args)
+      {
+          int n = Integer.parseInt(args[0]);
+          double angle= 360.00/n;
+          float p =Float.parseFloat(args[1]);
+                    
+          StdDraw.setXscale(-1.0, 1.0);
+          StdDraw.setYscale(-1.0, 1.0);
+          StdDraw.enableDoubleBuffering();
+          
+         //set array of points
+          double [][]points=new double[n][2];
+          
+          //fill the array of points
+          double a=0.0;
+          for (int i=0; i<n;i++)
+          {             
+              points[i][0]= 0.0 + 0.5*Math.cos(a);
+              points[i][1] = 0.0 + 0.5*Math.sin(a);
+              a+=angle;
+          }
+         
+          //draw points from array
+          for(int i=0;i<n; i++)
+          {
+              StdDraw.point(points[i][0],points[i][1]);
+              StdDraw.show();
+          }
+          
+          //iterate over each point and draw lines if its within probability
+          for(int i=0;i<n;i++)
+              {
+               for(int j=0;j<n;j++)
+               {
+                if (Math.random()<=p)
+                {
+                   StdDraw.line(points[i][0],points[i][1],points[j][0],points[j][1]);
+                   StdDraw.show();
+                }
+               }
+          }
+          
+
+          
+      }
+}
